2fa3675ca7f1873a490b1aa90fed0cf4403e1d13
Eric Dodémont <eric.dodemont@skynet.be>
2024-04-19 12:10:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,27 +3,18 @@ import jq
 from dotenv import load_dotenv
 from langchain_community.document_loaders import JSONLoader
 from langchain_openai import OpenAIEmbeddings
-from langchain_astradb import AstraDBVectorStore
+from langchain_chroma import Chroma
 
 load_dotenv()
 
-ASTRA_DB_APPLICATION_TOKEN = os.environ.get("ASTRA_DB_APPLICATION_TOKEN")
-ASTRA_DB_API_ENDPOINT = os.environ.get("ASTRA_DB_API_ENDPOINT")
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 
-file_path = "commons-ds1-wps-202404052059.json"
-collection_name = "bmae_json2"
+file_path = "balat-ds1c-wcc-cheerio-ex_2024-04-06_09-05-15-262.json"
+collection_name = "bmae-json"
 
 loader = JSONLoader(file_path=file_path, jq_schema=".[]", text_content=False)
 documents = loader.load()
 
-embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
 
-astravstore = AstraDBVectorStore(
-    embedding=embeddings,
-    collection_name=collection_name,
-    token=os.environ["ASTRA_DB_APPLICATION_TOKEN"],
-    api_endpoint=os.environ["ASTRA_DB_API_ENDPOINT"],
-)
-
-astra_ids = astravstore.add_documents(documents)
+db = Chroma.from_documents(documents, embeddings, persist_directory="./chromadb/bmae-json")
